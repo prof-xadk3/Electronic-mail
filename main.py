479811e3bd8acc560f08e972389af665817aff05
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-06 23:49:02 +0545

    .

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(data)
+
+
+if __name__ == "__main__":
+    __import__("uvicorn").run("main:api", host="0.0.0.0", port=55555)
