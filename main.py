4b3da19e16e6d33b13f6293825daedf9e595acd6
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-07 01:26:19 +0545

    .

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 #!/shebang
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import RedirectResponse
 
-# from fastapi.responses import JSONResponse
+from rsa import decrypt, encrypt
 
+
+# from fastapi.responses import JSONResponse
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def Ecrypt(self, websocket: WebSocket, plain):
+        await websocket.accept()
+        await self.broadcast(encrypt(plain, [111111111, 111111111]))
+
+    async def Dcrypt(self, websocket: WebSocket, plain):
+        await websocket.accept()
+        await self.broadcast(decrypt(plain, [111111111, 111111111]))
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 api = FastAPI()
 
 
@@ -12,13 +42,31 @@ def return_docs():
     return RedirectResponse("/docs")
 
 
+"""
 # relay here.
-@api.websocket("/ws")
+@api.websocket("/wz")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(data)
+
+"""
+
+
+@api.websocket("/ws/{cli_id}")
+async def websocket_endpoint(websocket: WebSocket, cli_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            d = encrypt(str(data), [111111111, 111111111])
+            print(d)
+            await manager.send_personal_message(f"Pv! {d}", websocket)
+            await manager.broadcast(f"Client #{cli_id} tells {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{cli_id} left the relay.")
 
 
 if __name__ == "__main__":
